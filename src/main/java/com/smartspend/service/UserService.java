174a1fd6dd7341a427277bd875dd63b32433347f
Aarti Gore <aartigore2005@gmail.com>
2026-08-18 23:55:27 +0530

    User entity added

---
+++ src/main/java/com/smartspend/service/UserService.java
@@ -0,0 +1,16 @@
+package com.smartspend.service;
+import com.smartspend.entity.User;
+import com.smartspend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+@Service
+public class UserService {;
+
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    public Optional<User> findByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+}
