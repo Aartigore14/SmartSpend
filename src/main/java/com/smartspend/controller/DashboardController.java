3192ea82017b77bd5e54ce8f68893445c5171a27
Aarti Gore <aartigore2005@gmail.com>
2026-09-26 19:48:37 +0530

    Fix dashboard API route

--- src/main/java/com/smartspend/controller/DashboardController.java
+++ src/main/java/com/smartspend/controller/DashboardController.java
@@ -1,18 +1,22 @@
 package com.smartspend.controller;
+
 import com.smartspend.dto.DashboardResponse;
 import com.smartspend.service.DashboardService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/dashboard")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173")
 public class DashboardController {
+
     private final DashboardService dashboardService;
-    public DashboardController(DashboardService dashboardService){
+
+    public DashboardController(DashboardService dashboardService) {
         this.dashboardService = dashboardService;
     }
-    @GetMapping("/users/{userId}")
-    public DashboardResponse getDashboard(@PathVariable Long userId){
+
+    @GetMapping("/user/{userId}")
+    public DashboardResponse getDashboard(@PathVariable Long userId) {
         return dashboardService.getDashboard(userId);
     }
 }
